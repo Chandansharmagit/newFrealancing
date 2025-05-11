a9f8c4e6627e9053115d716cafe250ccf7002b7f
chandan sharma <utamsharma57@gmail.com>
2025-05-11 21:26:06 +0545

    done testing

--- src/toursComponent/trackWhatsAppRequest.js
+++ src/toursComponent/trackWhatsAppRequest.js
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "https://backendtravelagency.onrender.com/";
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "https://backendtravelagency.onrender.com";
 
 /**
  * Track when users click to contact via WhatsApp
